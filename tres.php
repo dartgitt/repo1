bdba4ede0a6ef645c3c07fb65c2028218ecbe9ed
dartgitt <dartito77@gmail.com>
2022-02-14 13:21:46 +0100

    agregando tres.php

---
+++ tres.php
@@ -0,0 +1,7 @@
+<?php
+        echo "<h1>Bienvenidos a App I</h1>";
+        echo "<br>";
+        echo "<a href='dos.php'>Haz click para ir a dos.php</a>";
+        echo "<br>";
+        echo "<a href='cuatro.php'>Haz click para ir a cuatro.php</a>";
+?>
